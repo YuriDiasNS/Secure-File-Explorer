3906df82cbc81f49d089a78b4fbeb1b2fbd9bff2
Yuri-DNS <yurdias9@gmail.com>
2025-12-15 19:17:32 -0300

    Adiciona arquivos de configuração do Maven e estrutura inicial do projeto

---
+++ src/main/java/com/yuridiasns/secure_file_explorer_backend/SecureFileExplorerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yuridiasns.secure_file_explorer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileExplorerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFileExplorerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuridiasns/secure_file_explorer_backend/SecureFileExplorerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuridiasns.secure_file_explorer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureFileExplorerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
