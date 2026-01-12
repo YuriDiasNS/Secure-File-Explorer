52abf27260e1e06fb229b1f905747ff812cea90e
Yuri-DNS <yurdias9@gmail.com>
2026-01-12 18:21:52 -0300

    Corrige os imports para usar o pacote correto de fileExplorer nas classes ApiResponse, ExplorerResponse, FileInfoResponse, DirectoryView e FileView

--- src/main/java/com/yuridiasns/secure_file_explorer_backend/service/FileExplorerService.java
+++ src/main/java/com/yuridiasns/secure_file_explorer_backend/service/FileExplorerService.java
@@ -4,10 +4,10 @@ import com.yuridiasns.secure_file_explorer_backend.config.ExplorerProperties;
 import com.yuridiasns.secure_file_explorer_backend.exception.BadRequestException;
 import com.yuridiasns.secure_file_explorer_backend.exception.NotFoundException;
 import com.yuridiasns.secure_file_explorer_backend.exception.SecurityViolationException;
-import com.yuridiasns.secure_file_explorer_backend.model.Response.ExplorerResponse;
-import com.yuridiasns.secure_file_explorer_backend.model.Response.FileInfoResponse;
-import com.yuridiasns.secure_file_explorer_backend.model.View.DirectoryView;
-import com.yuridiasns.secure_file_explorer_backend.model.View.FileView;
+import com.yuridiasns.secure_file_explorer_backend.model.fileExplorer.Response.ExplorerResponse;
+import com.yuridiasns.secure_file_explorer_backend.model.fileExplorer.Response.FileInfoResponse;
+import com.yuridiasns.secure_file_explorer_backend.model.fileExplorer.View.DirectoryView;
+import com.yuridiasns.secure_file_explorer_backend.model.fileExplorer.View.FileView;
 import com.yuridiasns.secure_file_explorer_backend.security.PathSanitizer;
 
 import org.springframework.core.io.FileSystemResource;
