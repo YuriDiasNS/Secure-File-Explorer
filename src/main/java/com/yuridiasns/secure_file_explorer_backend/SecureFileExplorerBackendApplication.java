e388482ee1eca94f61b54bddf027268e1a52e7a8
Yuri-DNS <yurdias9@gmail.com>
2026-03-02 14:57:21 -0300

    feat: adicionar propriedades de configuração para download externo

--- src/main/java/com/yuridiasns/secure_file_explorer_backend/SecureFileExplorerBackendApplication.java
+++ src/main/java/com/yuridiasns/secure_file_explorer_backend/SecureFileExplorerBackendApplication.java
@@ -2,11 +2,12 @@ package com.yuridiasns.secure_file_explorer_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+
 @SpringBootApplication
-@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 @EnableAsync
 public class SecureFileExplorerBackendApplication {
 
--- src/main/java/com/yuridiasns/secure_file_explorer_backend/config/ExplorerProperties.java
+++ src/main/java/com/yuridiasns/secure_file_explorer_backend/config/ExplorerProperties.java
@@ -1,11 +1,9 @@
 package com.yuridiasns.secure_file_explorer_backend.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
 
-@Component
 @ConfigurationProperties(prefix = "explorer")
 public class ExplorerProperties {
 
